85354e4ee78c7cc35de3cbd13321e6a9c37a7fcb
tranmanhgh <manhnus@gmail.com>
2016-04-28 18:42:14 +0800

    add code to test Manh's web, change permalink

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 module.exports = function(robot) {
-  robot.respond(/hello world/, function(msg, done) {
-    msg.reply("hello back", done);
-  });
+	robot.respond(/test Manh's web/, function(msg, done) {
+		robot.http("http://139.59.230.129/").get()(function(err, res, body) {
+			if (err) {
+				msg.reply("Encountered an error :( " + err, done);
+			}
+		    msg.reply("It's working!", done);
+		});
+	});
 };
